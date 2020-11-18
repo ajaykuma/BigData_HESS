06aca2e24adfb3e6656178fda30a1ad3590a6397
Aj <ajaykuma24@gmail.com>
2020-11-18 13:27:41 +0100

    updated

---
+++ Hadoop_Spark_Setup/Oozie_Setup/workflow1/hive-6a11.sql
@@ -0,0 +1,5 @@
+USE default;
+
+use mydb;
+select * from testtbl2 where id > 333 order by id desc;
+select * from testtbl3 where id < 333 order by id asc;
---
+++ Hadoop_Spark_Setup/Oozie_Setup/workflow1/hive-8bdc.sql
@@ -0,0 +1,4 @@
+USE default;
+
+use mydb;
+create table testtbl3 as select * from testtbl2;
