a928db43b6bbee7a174e76e55c872c07c7b92aed
Aj <ajaykuma24@gmail.com>
2021-02-10 00:07:07 +0100

    updated'

--- Spark_n_Scala/SparkApplicationBasicExamples/Pyspark/SampleProject2/sampleapp3.py
+++ Spark_n_Scala/SparkApplicationBasicExamples/Pyspark/SampleProject2/sampleapp3.py
@@ -4,9 +4,12 @@ from pyspark.sql.types import IntegerType,StringType, StructType, StructField
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.master("local").appName("sparkdfex").getOrCreate()
 inputpath = "Data"
-schema = StructType([StructField("name",StringType(),True),
+"""schema = StructType([StructField("name",StringType(),True),
                      StructField("age",IntegerType())])
 
-inputDF = (spark.read.schema(schema).json(inputpath))
+inputDF = (spark.read.schema(schema).json(inputpath))"""
+inputDF = spark.read.json(inputpath)
 
 inputDF.show()
+inputDF.printSchema()
+print(inputDF.count())
