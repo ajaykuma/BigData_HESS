ccbf90b70a47600b270b612ccc968c61838b2a51
ajay <ajaykuma24@gmail.com>
2021-07-07 12:54:30 +0200

    updated

---
+++ Hadoop_OtherServices_Setup/Sqoop_n_flume/Flume/flumeexec.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bin/flume-ng agent --conf-file my.conf Dflume.root.logger=DEBUG,console -n a1
--- Hadoop_OtherServices_Setup/Sqoop_n_flume/Flumeconf2/flumeexec.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-bin/flume-ng agent --conf-file conf/my.conf Dflume.root.logger=DEBUG,console -n a1
